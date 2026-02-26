f6686fa3c7756d13f8e337201c3af5cccee7b019
Daves Macbook <el-davo@Davess-MacBook-Pro.local>
2026-02-26 15:18:58 +0000

    Added graphql

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HelloResolver } from './graphql/hello.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // GraphQL endpoint is available at `/graphql`.
+      path: 'graphql',
+      autoSchemaFile: true,
+      introspection: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HelloResolver],
 })
 export class AppModule {}
